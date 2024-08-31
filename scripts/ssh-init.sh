93de23c2ac2d7ad0826f2e8bbc923bc00d516e0c
mottzz87 <57702920+mottzz87@users.noreply.github.com>
2024-09-01 01:30:47 +0800

    Delete scripts/ssh-init.sh

--- scripts/ssh-init.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo -i | echo "root:wN4@xhA5WxKj5nhO" | sudo chpasswd root && sudo sed -i 's/^#?PermitRootLogin./PermitRootLogin yes/g' /etc/ssh/sshd_config && sudo sed -i 's/^#?PasswordAuthentication./PasswordAuthentication yes/g' /etc/ssh/sshd_config && sudo service ssh restart

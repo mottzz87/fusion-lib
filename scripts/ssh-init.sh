c65e5bc62a65fbb4898171883f8186cdd68db643
mottzz87 <57702920+mottzz87@users.noreply.github.com>
2024-09-01 01:21:58 +0800

    Update ssh-init.sh

--- scripts/ssh-init.sh
+++ scripts/ssh-init.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-sudo -i 
-echo "root:wN4@xhA5WxKj5nhO"
-sudo chpasswd root 
-sudo sed -i 's/^#?PermitRootLogin./PermitRootLogin yes/g' /etc/ssh/sshd_config 
-sudo sed -i 's/^#?PasswordAuthentication./PasswordAuthentication yes/g' /etc/ssh/sshd_config 
-sudo service ssh restart
+sudo -i | echo "root:wN4@xhA5WxKj5nhO" | sudo chpasswd root && sudo sed -i 's/^#?PermitRootLogin./PermitRootLogin yes/g' /etc/ssh/sshd_config && sudo sed -i 's/^#?PasswordAuthentication./PasswordAuthentication yes/g' /etc/ssh/sshd_config && sudo service ssh restart
